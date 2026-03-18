d1b6cc17c2181bdf2ae07b4ece85cd13c41048f0
Viral Dobariya <viraldobariya33@gmail.com>
2026-03-18 11:58:20 +0530

    Add S3 backend configuration for Terraform

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,12 @@ terraform {
       version = "6.36.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "three-tier-tf-state-bucke"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
 }
