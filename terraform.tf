cb2e52a299869260a281d970095952baf4f49753
Viral Dobariya <viraldobariya33@gmail.com>
2026-03-18 12:20:36 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -5,12 +5,4 @@ terraform {
       version = "6.36.0"
     }
   }
-
-  backend "s3" {
-    bucket         = "three-tier-tf-state-bucke"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
-  }
 }
